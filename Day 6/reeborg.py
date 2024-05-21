80d2babab70904cccb9b2686053927462ab8b280
Sebastian Morera <sebastianmorera@Sebastians-MBP.hitronhub.home>
2024-05-21 10:18:45 -0400

    Day 1 to day 7 done

---
+++ Day 6/reeborg.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    print("This day had no code to be written, the challenge is to complete on a web page some challenges of a game!")
+    print(r"""https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json""")
