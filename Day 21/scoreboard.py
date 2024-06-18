3bff4f8be41f1cd27b18a2a1bf81accc78a3ab58
Sebastian Morera <sebastianmorera@Sebastians-MBP.hitronhub.home>
2024-06-18 14:28:03 -0400

    Day 22 done, fixed some typos in Day 21

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -11,15 +11,15 @@ class ScoreBoard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
-        self.write(f"GAME OVER!", align="center", font=("Arial", 24, "normal"))
+        self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
--- Day 21/snake_game.py
+++ Day 21/snake_game.py
@@ -1,8 +1,8 @@
-import time
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import ScoreBoard
+import time
 
 
 def snake_game():
@@ -40,7 +40,7 @@ def snake_game():
             score.game_over()
 
         # Detect collision with tail
-        for segment in snake.segments:
+        for segment in snake.segments[1:]:
             if snake.head.distance(segment) < 10:
                 game_is_on = False
                 score.game_over()
