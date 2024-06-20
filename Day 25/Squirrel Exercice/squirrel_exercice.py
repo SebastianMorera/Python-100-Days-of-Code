e97eba87ed0522cd80cc3e3ce7da332409aa34ae
Sebastian Morera <sebastianmorera@Sebastians-MBP.hitronhub.home>
2024-06-20 15:56:54 -0400

    Day 25 done

---
+++ Day 25/Squirrel Exercice/squirrel_exercice.py
@@ -0,0 +1,19 @@
+import pandas
+
+
+def squirrel():
+    squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240620.csv")
+    gray_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+    cinnamon_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+    black_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+
+    color_dict = {
+        "Fur Color": ["Gray", "Cinnamon", "Black"],
+        "Count": [gray_count, cinnamon_count, black_count]
+    }
+    data_color = pandas.DataFrame(color_dict)
+    data_color.to_csv("squirrel_count.csv")
+
+
+if __name__ == '__main__':
+    squirrel()
